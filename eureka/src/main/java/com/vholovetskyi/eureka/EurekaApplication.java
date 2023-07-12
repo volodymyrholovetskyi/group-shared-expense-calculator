d796f6eafea9b69cf12b0cddf2fcb9e8f974afe8
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2023-07-12 17:02:24 +0200

    Configuartion application.properties

--- eureka/src/main/java/com/vholovetskyi/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/vholovetskyi/eureka/EurekaApplication.java
@@ -2,8 +2,10 @@ package com.vholovetskyi.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
